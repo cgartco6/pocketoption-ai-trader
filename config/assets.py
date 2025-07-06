e781d4461d8243f5de072ad7a5f6c98b507941ec
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-07-06 22:15:26 +0200

    Create assets.py

---
+++ config/assets.py
@@ -0,0 +1,16 @@
+# 92%+ payout assets on PocketOption
+high_payout:
+  forex:
+    - "EUR/USD"
+    - "GBP/USD"
+    - "USD/JPY"
+    - "AUD/USD"
+    - "USD/CAD"
+  crypto:
+    - "BTC/USD"
+    - "ETH/USD"
+    - "LTC/USD"
+    - "XRP/USD"
+  indices:
+    - "US500"
+    - "US30"
